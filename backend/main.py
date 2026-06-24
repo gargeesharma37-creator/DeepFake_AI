3c9d01a010cfc36c1e25836edee124af91c348f4
Gargee Sharma <gargeesharma37@gmail.com>
2026-06-24 22:34:21 +0530

    Day 3 AI backend completed

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from backend.voice_detector import predict_voice
 
 app = FastAPI()
 
@@ -6,4 +7,29 @@ app = FastAPI()
 def home():
     return {
         "message": "DeepFake AI Backend Running"
+    }
+
+@app.get("/predict")
+def predict():
+    
+    sample_features = [
+        -247.76,
+        77.11,
+        -10.09,
+        29.62,
+        -6.38,
+        -4.65,
+        -17.60,
+        -14.64,
+        -11.31,
+        -21.16,
+        -8.75,
+        -11.13,
+        -14.56
+    ]
+
+    result = predict_voice(sample_features)
+
+    return {
+        "prediction": result
     }
